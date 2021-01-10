e74774cef82de5c3b1ac947e4418c58119fbf699
ryanlu2240 <kevinlu2240@gmail.com>
2021-01-11 03:03:29 +0800

    .

--- Grid_Paths.cpp
+++ Grid_Paths.cpp
@@ -10,26 +10,80 @@
 
 using namespace std;
 
-char board[2][2];
+bool board[7][7];
+ll c = 0;
+ll step = 0;
+string s;
 
-void print_board(int size){
-    for(int i=0;i<size;i++){
-        for(int j=0;j<size;j++){
-            cout<<board[i][j] <<' ';
-        }
-        cout<<endl;
+void search(int row, int col){
+    if(step == 48){
+        if(col==0 && row==6) c++;
+        return;
     }
-    cout<<endl;
-
+    if(board[row][col]) return;
+    if((row>0 && !board[row-1][col]) && (row+1 < 7 && !board[row+1][col]) && !(col>0 && !board[row][col-1]) && !(col+1 < 7 && !board[row][col+1])) return; //can only go up and down
+    if((col>0 && !board[row][col-1]) && (col+1 < 7 && !board[row][col+1]) && !(row>0 && !board[row-1][col]) && !(row+1 < 7 && !board[row+1][col])) return; //can only go left ans right
+    if(board[6][0] && step<48) return; //the destination board[6][0] is already reserved
+    board[row][col] = true;
+    if(s[step]!='?'){
+        if(s[step] == 'U'){
+            if(row>0 && !board[row-1][col]){ //UP
+                step++;
+                search(row-1, col);
+                step--;
+            }
+        }else if(s[step] == 'D'){
+            if(row+1 < 7 && !board[row+1][col]){ //DOWN
+                step++;
+                search(row+1, col);
+                step--;
+            }
+        }else if(s[step] == 'L'){
+            if(col>0 && !board[row][col-1]){ //LEFT
+                step++;
+                search(row, col-1);
+                step--;
+            }
+        }else if(s[step] == 'R'){
+            if(col+1<7 && !board[row][col+1]){ //RIHGT
+                step++;
+                search(row, col+1);
+                step--;
+            }
+        }
+        board[row][col] = false;
+        return;
+    }
+    if(row>0 && !board[row-1][col]){ //UP
+        step++;
+        search(row-1, col);
+        step--;
+    }
+    if(row+1 < 7 && !board[row+1][col]){ //DOWN
+        step++;
+        search(row+1, col);
+        step--;
+    }
+    if(col>0 && !board[row][col-1]){ //LEFT
+        step++;
+        search(row, col-1);
+        step--;
+    }
+    if(col+1<7 && !board[row][col+1]){ //RIHGT
+        step++;
+        search(row, col+1);
+        step--;
+    }
+    board[row][col] = false;
 }
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    memset(board, '.', 4*sizeof(char));
-    print_board(2);
-
-
+    cin>>s;
+    memset(board, false, 49*sizeof(bool));
+    search(0,0);
+    cout<<c<<endl;
 
 }
--- a.cpp
+++ a.cpp
@@ -4,131 +4,48 @@
 #include <math.h>
 using namespace std;
 
-// Program to print board
-void printBoard(vector< vector<char> >& board)
-{
-for (auto row : board)
-{
-	cout<<"|";
-	for (auto ch : row)
-	{
-	cout<<ch<<"|";
-	}
-	cout<<endl;
-}
-}
-
-// Program to solve N Quuens problem
-void solveBoard(vector<vector<char> >& board, int row,
-				int rowmask, int ldmask, int rdmask, int& ways)
-{
-
-int n = board.size();
-
-// All_rows_filled is a bit mask having all N bits set
-int all_rows_filled = (1 << n) - 1;
-
-// If rowmask will have all bits set, means queen has been
-// placed successfully in all rows and board is diplayed
-if (rowmask == all_rows_filled)
-{
-	ways++;
-	cout<<"=====================\n";
-	cout<<"Queen placement - "<<ways<<endl;
-	cout<<"=====================\n";
-	printBoard(board);
-	return;
-}
-
-// We extract a bit mask(safe) by rowmask,
-// ldmask and rdmask. all set bits of 'safe'
-// indicates the safe column index for queen
-// placement of this iteration for row index(row).
-int safe = all_rows_filled & (~(rowmask |
-								ldmask | rdmask));
-while (safe)
-{
-
-	// Extracts the right-most set bit
-	// (safe column index) where queen
-	// can be placed for this row
-	int p = safe & (-safe);
-	int col = (int)log2(p);
-	board[row][col] = 'Q';
-
-	// This is very important:
-	// we need to update rowmask, ldmask and rdmask
-	// for next row as safe index for queen placement
-	// will be decided by these three bit masks.
-
-	// We have all three rowmask, ldmask and
-	// rdmask as 0 in beginning. Suppose, we are placing
-	// queen at 1st column index at 0th row. rowmask, ldmask
-	// and rdmask will change for next row as below:
-
-	// rowmask's 1st bit will be set by OR operation
-	// rowmask = 00000000000000000000000000000010
-
-	// ldmask will change by setting 1st
-	// bit by OR operation and left shifting
-	// by 1 as it has to block the next column
-	// of next row because that will fall on left diagonal.
-	// ldmask = 00000000000000000000000000000100
-
-	// rdmask will change by setting 1st bit
-	// by OR operation and right shifting by 1
-	// as it has to block the previous column
-	// of next row because that will fall on right diagonal.
-	// rdmask = 00000000000000000000000000000001
-
-	// these bit masks will keep updated in each
-	// iteration for next row
-	solveBoard(board, row+1, rowmask|p, (ldmask|p) << 1,
-			(rdmask|p) >> 1, ways);
-
-	// Reset right-most set bit to 0 so,
-	// next iteration will continue by placing the queen
-	// at another safe column index of this row
-	safe = safe & (safe-1);
-
-	// Backtracking, replace 'Q' by ' '
-	board[row][col] = ' ';
-}
-return;
-}
-
-// Driver Code
 int main()
 {
-// Board size
-int n = 8;
-int ways = 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-vector< vector<char> > board;
-for (int i = 0; i < n-1; i++)
-{
-	vector<char> tmp;
-	for (int j = 0; j < n; j++)
-	{
-	tmp.push_back(' ');
-	}
-	board.push_back(tmp);
-}
-vector<char> tmp;
-for (int j = 0; j < n-1; j++)
-{
-tmp.push_back(' ');
-}
-tmp.push_back('Q');
-board.push_back(tmp);
-int rowmask = 0, ldmask = 0, rdmask = 0;
-int row = 0;
+    long long int n;
+    cin>>n;
+    string s;
+    long long int mod = 1e9+7;
 
-// Function Call
-solveBoard(board, row, rowmask, ldmask, rdmask, ways);
-cout<<endl<<"Number of ways to place "<<n<<" queens : "
-	<<ways<<endl;
+    vector<vector<long long int> > dp(n+1,vector<long long int> (n+1,0));
+    dp[0][0] = 1;
 
-return 0;
+    for(long long int i=0;i<n;i++)
+    {
+        cin>>s;
+
+        for(long long int j=0;j<n;j++)
+        {
+            if(s[j] != '*')
+            {
+                if(i>0)
+                {
+                dp[i][j] += dp[i-1][j];
+                dp[i][j] %= mod;
+                }
+                if(j>0)
+                {
+                dp[i][j] += dp[i][j-1];
+                dp[i][j] %= mod;
+                }
+            }
+
+            else
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    cout<< ( n>0 ? (dp[n-1][n-1] % mod) : -1);
+
+    return 0;
 }
-// This code is contributed by Nikhil Vinay
